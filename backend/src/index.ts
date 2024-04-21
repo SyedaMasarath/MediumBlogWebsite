4dbdb2fb76d53f74151136f6eb873a979b8d913a
masarath <zmasarath@gmail.com>
2024-04-21 18:05:59 +0530

    update .ignore

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,10 @@
 import { Hono } from 'hono';
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
+const prisma = new PrismaClient({
+    datasourceUrl: env.DATABASE_URL,
+}).$extends(withAccelerate())
 // Create the main Hono app
 const app = new Hono();
 
